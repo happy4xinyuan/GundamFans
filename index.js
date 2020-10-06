420f889cecb275517aaecde532d17752da93067b
Xinyuan-Cai <779121147@qq.com>
2020-10-06 17:38:44 +0800

    add mLab

--- index.js
+++ index.js
@@ -46,6 +46,6 @@ app.use(indexRouter);
 app.use(gundammsRouter);
 app.use(commentRouter);
 
-app.listen(3000,function(){
+app.listen(process.env.PORT,process.env.IP,function(){
     console.log("gundam runnig");
 })
